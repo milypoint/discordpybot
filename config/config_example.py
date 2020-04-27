d4f5e7c126434f363dc57b0635d6d95c3f618835
Andrii Tokar <milypointo@gmail.com>
2020-04-27 03:12:45 +0300

    ffs...

--- config/config_example.py
+++ config/config_example.py
@@ -5,3 +5,4 @@ token = 'Your token here'
 LOG_FILE = 'discord.log'
 WORK_PATH = str(Path(os.path.dirname(os.path.abspath(__file__))).parents[0])
 DEBUG = True
+CONFIG_PATH = str(os.path.dirname(os.path.abspath(__file__)))
