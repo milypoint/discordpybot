42b74a88d0c200619f2fcaaf78680ad84bea8a0f
Andrii Tokar <milypointo@gmail.com>
2020-05-04 16:58:11 +0300

    added missed line to config_example

--- config/config_example.py
+++ config/config_example.py
@@ -1,8 +1,13 @@
 import os
+import sys
 from pathlib import Path
+
+import objects.config.config_handler as ch
 
 token = 'Your token here'
 LOG_FILE = 'discord.log'
 WORK_PATH = str(Path(os.path.dirname(os.path.abspath(__file__))).parents[0])
 DEBUG = True
 CONFIG_PATH = str(os.path.dirname(os.path.abspath(__file__)))
+
+sys.modules[__name__] = ch.Config(globals())
